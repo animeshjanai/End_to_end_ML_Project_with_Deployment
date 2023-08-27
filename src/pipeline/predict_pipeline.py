a05d523d14b29907a4d2a86f746d9c07dc714ddb
animeshjanai <animesh.janai@gmail.com>
2023-08-28 00:04:59 +0530

    scikit learn version change

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import pandas as pd
 from src.exception import CustomException
 from src.utils import load_object
@@ -9,8 +10,8 @@ class PredictPipeline:
 
     def predict(self,features):
         try:
-            model_path = 'artifacts\model.pkl'
-            preprocessor_path = 'artifacts\preprocessor.pkl'
+            model_path = os.path.join("artifacts","model.pkl")
+            preprocessor_path = os.path.join("artifacts","preprocessor.pkl")
             model=load_object(file_path=model_path)
             preprocessor=load_object(file_path=preprocessor_path)
             data_scaled=preprocessor.transform(features)
